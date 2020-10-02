b075118c7e355d6f8d09d9bfb939af8571c68f15
Thomas <tomasmontoya123@gmail.com>
2020-10-02 14:02:07 -0500

    [Fix] Heroku deploy files

--- prgx/settings.py
+++ prgx/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!*xvnfw)aa!&c!0ou)xdnuv=^(sl&^*8*6i#eg*uos#=cn0u$6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
